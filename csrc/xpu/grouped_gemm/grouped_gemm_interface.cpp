c2c23726274206884e7d6d80174e608552b2e56f
frost-intel <frost.mitchell@intel.com>
2026-07-10 03:18:38 -0700

    Fix scale var name
    Signed-off-by: frost-intel <frost.mitchell@intel.com>

--- csrc/xpu/grouped_gemm/grouped_gemm_interface.cpp
+++ csrc/xpu/grouped_gemm/grouped_gemm_interface.cpp
@@ -20,6 +20,10 @@ torch::Tensor cutlass_grouped_gemm_interface(
     int64_t N,
     int64_t K,
     int64_t num_experts) {
+  const auto B_dtype = ptr_B.dtype();
+  const bool is_B_fp8block =
+      (B_dtype == at::kFloat8_e4m3fn || B_dtype == at::kFloat8_e5m2) &&
+      ptr_B_scale.has_value() && ptr_B_scale->dim() == 3;
   if (vllm::xpu::force_xe_default_kernel()) {
 #ifdef VLLM_XPU_ENABLE_XE_DEFAULT
     int64_t groups = num_experts;
